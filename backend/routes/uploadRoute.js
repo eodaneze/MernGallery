d6991a8c2dc9f6db9e44d8a7d6f25682f7460a09
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-24 06:22:15 -0700

    creating a multermiddleware and calling it in the upload route

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
+const uploadMiddleware = require('../middleware/MulterMiddleware');
 
 const router = Router();
 
-router.post("/save", async(req, res) => {
-    res.send("Handling post request")
+router.post("/save", uploadMiddleware.single("photo"), (req, res) => {
+
 })
 module.exports = router
