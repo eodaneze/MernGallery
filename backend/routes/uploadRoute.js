58d6cac2566d48f81af55956bf0ff56e70c39213
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-24 06:33:39 -0700

    creating a a post for image upload

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -4,6 +4,13 @@ const uploadMiddleware = require('../middleware/MulterMiddleware');
 const router = Router();
 
 router.post("/save", uploadMiddleware.single("photo"), (req, res) => {
-
+  const photo = req.file.filename
+  console.log(photo);
+  uploadModel.create({photo})
+  .then((data) => {
+    console.log("Uploaded successfully");
+    console.log(data);
+    res.send(data);
+  }).catch(err => console.log(err.message));
 })
 module.exports = router
