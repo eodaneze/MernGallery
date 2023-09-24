48b88dc53743c6792196955ed6e13911328a6c06
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-24 07:34:55 -0700

    adding the upload image button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Button from './components/Button'
 
 const App = () => {
   return (
     <div>
        <Navbar />
+       <Button />
     </div>
   )
 }
--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import {AiFillPlusCircle} from 'react-icons/ai'
 const Button = () => {
   return (
-    <div>Button</div>
+    <div>
+        <label className='button' htmlFor='file_picker'>
+            <AiFillPlusCircle />
+            <input type="file" name='file_picker' id='file_picker' hidden />
+        </label>
+    </div>
   )
 }
 
