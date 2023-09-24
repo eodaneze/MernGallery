e6bdb6d98fccf7bda5adacb1e30670cfd21a2470
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-24 07:23:53 -0700

    designing the navbar componets

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="nav__logo">
+            Photo Gallery App
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
